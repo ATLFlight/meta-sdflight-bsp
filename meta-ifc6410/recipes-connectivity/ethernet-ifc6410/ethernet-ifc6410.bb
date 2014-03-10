6ead67e9dd0f676bc96d0e228d43f2c798d3f4b1
Rahul Anand <ranand@codeaurora.org>
2014-03-10 12:45:21 -0700

    Fixed MAC Addr handling to decouple eth0 & wlan0
    Added if-pre-up.d/eth0-config-macaddr that configures the eth0 MAC addr. It gets installed
    with a placeholder, that gets substituted with the real MAC address by the script that pulls
    them from Android.
    With this change, the script to configure ether MACs, that was being generated by the above
    script is no longer needed. That script was also being run every time, which resulted in eth0
    being brought down if wlan0 was brought up.
    
    Change-Id: Id82229445217b1cff5a33adaa99ee27b40dce890

--- meta-ifc6410/recipes-connectivity/ethernet-ifc6410/ethernet-ifc6410.bb
+++ meta-ifc6410/recipes-connectivity/ethernet-ifc6410/ethernet-ifc6410.bb
@@ -5,11 +5,15 @@ LIC_FILES_CHKSUM = "file://${COREBASE}/meta-qr-linux/COPYING;md5=7b4fa59a65c2beb
 PR = "r0"
 PV = "1.0"
 
-SRC_URI = "file://eth0.cfg"
+SRC_URI = "\
+   file://eth0.cfg \
+   file://eth0-config-macaddr"
 
 PACKAGES = "${PN}"
 
 do_install() {
     install -d ${D}${sysconfdir}/network/interfaces.d
     install -m 644 ${WORKDIR}/eth0.cfg ${D}${sysconfdir}/network/interfaces.d
+    install -d ${D}${sysconfdir}/network/if-pre-up.d
+    install -m 755 ${WORKDIR}/eth0-config-macaddr ${D}${sysconfdir}/network/if-pre-up.d
 }
--- meta-ifc6410/recipes-kernel/compat-wireless/compat-wireless/ifc6410-android2linux-macaddress.sh
+++ meta-ifc6410/recipes-kernel/compat-wireless/compat-wireless/ifc6410-android2linux-macaddress.sh
@@ -2,52 +2,59 @@
 
 t=/etc/network/
 
-out=/etc/network/eth0-init.sh
-
-if [ ! -f $t/macaddress.done ]; then
-   afw=/mnt/system/etc/firmware/ath6k/AR6004/hw3.0/
-   lfw=/lib/firmware/ath6k/AR6004/hw3.0/
-   mkdir -p /mnt/system
-   mount /dev/mmcblk0p12 /mnt/system/
-   mkdir -p $lfw
-   cp $afw/softmac.bin $lfw
-   cp $afw/fw.ram.bin $lfw
-   cp $afw/bdata.bin_sdio $lfw/bdata.bin
-   umount /mnt/system
-
-   afw=/mnt/system/etc/firmware/ath6k/AR6004/hw1.3/
-   lfw=/lib/firmware/ath6k/AR6004/hw1.3/
-   mkdir -p /mnt/system
-   mount /dev/mmcblk0p12 /mnt/system/
-   mkdir -p $lfw
-   cp $afw/softmac.bin $lfw
-   cp $afw/fw.ram.bin $lfw
-   cp $afw/bdata.bin_sdio $lfw/bdata.bin
-   umount /mnt/system
-
-
-   mkdir -p /mnt/userdata
-   mount /dev/mmcblk0p13 /mnt/userdata/
-   cp /mnt/userdata/eth0.sh $t
-   umount /mnt/userdata
-
-   MAC=`cat $t/eth0.sh  | grep hwaddr | sed 's/.*hwaddr //g'`
-   echo "ifconfig eth0 down" > $out
-   echo "ifconfig eth0 hw ether $MAC" >> $out
-   chmod 700 $out
-
-   echo 1 > $t/macaddress.done
+if [ -f $t/macaddress.done ]; then
+   # Nothing to do
+   exit 0
 fi
 
-. $out
+
+# First mount the right android partition to copy firmware
+mkdir -p /mnt/system || {
+    echo "[ERROR] Error creating mount point"
+    exit 1
+}
+
+mount /dev/mmcblk0p12 /mnt/system/ || {
+    echo "[ERROR] Error mounting the firmware parition"
+    exit 1
+}
+
+#  Copy both versions of the firmware. It's ok for one
+# of these to fail, in cases where either hw3.0 or hw1.3 dirs
+# don't exist
+afw=/mnt/system/etc/firmware/ath6k/AR6004/hw3.0/
+lfw=/lib/firmware/ath6k/AR6004/hw3.0/
+mkdir -p $lfw
+cp $afw/softmac.bin $lfw
+cp $afw/fw.ram.bin $lfw
+cp $afw/bdata.bin_sdio $lfw/bdata.bin
+
+afw=/mnt/system/etc/firmware/ath6k/AR6004/hw1.3/
+lfw=/lib/firmware/ath6k/AR6004/hw1.3/
+mkdir -p $lfw
+cp $afw/softmac.bin $lfw
+cp $afw/fw.ram.bin $lfw
+cp $afw/bdata.bin_sdio $lfw/bdata.bin
+
+umount /mnt/system
+
+# Now get the ethernet MAC address from the Android
+# userdata partition, and set that in our ethernet
+# config scripts
+mkdir -p /mnt/userdata || {
+    echo "[ERROR] Error creating mount point"
+    exit 1
+}
+mount /dev/mmcblk0p13 /mnt/userdata/ || {
+    echo "[ERROR] Error mounting the userdata partition"
+    exit 1
+}
+
+MAC=`cat /mnt/userdata/eth0.sh  | grep hwaddr | sed 's/.*hwaddr //g'`
+umount /mnt/userdata
+sed -i -e "s/@REPLACE_WITH_REAL_MAC_ADDR@/${MAC}/" /etc/network/if-pre-up.d/eth0-config-macaddr
+echo 1 > $t/macaddress.done
 
 
-
-exit
-
-# Start from scratch
-rm /etc/network/macaddress.done
-rm -fr /lib/firmware/ath6k
-rm /etc/network/eth0-init.sh
 
 
